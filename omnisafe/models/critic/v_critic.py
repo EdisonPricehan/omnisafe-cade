62dbdf58e1801e59d3c836310b7747ba0c37b8c2
Edison <princehan920453024@gmail.com>
2025-03-08 14:41:39 -0500

    use default identity activation for v_critic's final layer. v_critic is the cost estimator in CADE that accepts patchified semantic mask as observation.

--- omnisafe/models/critic/v_critic.py
+++ omnisafe/models/critic/v_critic.py
@@ -67,6 +67,7 @@ class VCritic(Critic):
             net = build_mlp_network(
                 sizes=[self._obs_dim, *self._hidden_sizes, 1],
                 activation=self._activation,
+                # output_activation='sigmoid',
                 weight_initialization_mode=self._weight_initialization_mode,
             )
             self.net_lst.append(net)
