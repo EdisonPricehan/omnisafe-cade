1e5e278b59307e2eab4d04f821e405b141b55b12
Edison <princehan920453024@gmail.com>
2025-03-10 20:22:45 -0400

    update env id of SRE medium.

--- examples/train_riverine.py
+++ examples/train_riverine.py
@@ -4,7 +4,7 @@ from omnisafe.envs.riverine_env import RiverineEnv
 
 
 if __name__ == '__main__':
-    env_id = 'Medium'
+    env_id = 'medium'
 
     agent = omnisafe.Agent('FOCOPS_CACD', env_id)
 
