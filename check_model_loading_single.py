010b18452a30cd5c29070af47128c3dcc23a3928
Edison-JetsonOrinNano <princehan920453024@gmail.com>
2025-07-29 15:33:05 -0400

    add more example paths for checking torch loading.

--- check_model_loading_single.py
+++ check_model_loading_single.py
@@ -7,7 +7,14 @@ import torch
 
 
 if __name__ == "__main__":
-    pth_path = 'examples/models/torch_save/epoch-350.pt'
+    # pth_path = 'examples/models/torch_save/epoch-350.pt'  # Successful
+    # pth_path = 'examples/models/torch_save/real-episode-000-hitl-True-loss-Indirect-20250629_165120.pt'  # Successful
+    # pth_path = 'examples/models/torch_save/wabash_0630/upstream/real-episode-001-hitl-True-loss-Indirect-20250630_101703.pt'  # Failed
+    # pth_path = 'examples/models/torch_save/real-episode-000-hitl-True-loss-Indirect-20250729_121811.pt'  # Successful
+    # pth_path = 'examples/models/torch_save/real-episode-001-hitl-True-loss-Indirect-20250729_121912.pt'  # Successful
+    # pth_path = 'examples/models/torch_save/real-episode-000-hitl-True-loss-Indirect-20250729_134513.pt'  # Successful
+    pth_path = 'examples/models/torch_save/real-episode-000-hitl-True-loss-Indirect-20250729_132336.pt'  # Successful
+
 
     if not os.path.exists(pth_path):
         raise FileNotFoundError(f"Model file {pth_path} does not exist.")
